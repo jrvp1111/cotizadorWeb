6d832e6f40123112087d5dd65abc58ab56efed71
jrvp1111 <jrvp1111@gmail.com>
2016-04-17 12:12:22 -0600

    se corrigio editar producto, no jalaba, pero ya

--- cotizador_web.sql
+++ cotizador_web.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-04-2016 a las 05:21:33
+-- Tiempo de generación: 17-04-2016 a las 20:11:15
 -- Versión del servidor: 10.1.8-MariaDB
 -- Versión de PHP: 5.6.14
 
@@ -42,8 +42,7 @@ CREATE TABLE `clientes` (
 --
 
 INSERT INTO `clientes` (`id_cte`, `comp_cte`, `nombcomer_cte`, `tel_cte`, `dir_cte`, `ciud_cte`, `edo_cte`, `fecha_cte`) VALUES
-(1, 'Casa Ley S.A. De C.V.', 'Casa Ley', '(667) 759 1000', 'Carretera Internacional Km 1434 Col. Infonavit Humaya C.P. 80020', 'CuliacÃ¡n', 'Sinaloa', '2016-04-12'),
-(2, 'Sergio Valenzuela Salazar', 'Distribuidora de Occidente', '(667) 716 4522', 'Rio Zuaque # 980 Col. Antonio Rosales C.P. 80230', 'CuliacÃ¡n', 'Sinaloa', '2016-04-15');
+(1, 'Casa Ley S.A. De C.V.', 'Casa Ley', '(667) 759 1000', 'Carretera Internacional Km 1434 Col. Infonavit Humaya C.P. 80020', 'CuliacÃ¡n', 'Sinaloa', '2016-04-12');
 
 -- --------------------------------------------------------
 
@@ -55,6 +54,9 @@ CREATE TABLE `marcas` (
   `id_mca` int(11) NOT NULL,
   `nomb_mca` varchar(100) NOT NULL,
   `origen_mca` varchar(50) NOT NULL,
+  `prov_mca` varchar(100) NOT NULL,
+  `tel_mca` varchar(20) NOT NULL,
+  `nota_mca` varchar(100) NOT NULL,
   `fecha_reg` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -62,14 +64,13 @@ CREATE TABLE `marcas` (
 -- Volcado de datos para la tabla `marcas`
 --
 
-INSERT INTO `marcas` (`id_mca`, `nomb_mca`, `origen_mca`, `fecha_reg`) VALUES
-(29, 'Deltatrak', 'Nacional', '2016-04-11'),
-(31, 'Extruflex', 'ImportaciÃ³n', '2016-04-11'),
-(32, 'Taylor', 'Nacional', '2016-04-12'),
-(33, 'Extech', 'Nacional', '2016-04-12'),
-(34, 'Escort Ilog', 'ImportaciÃ³n', '2016-04-14'),
-(35, 'Avaly', 'Nacional', '2016-04-15'),
-(36, 'Fluke', 'Nacional', '2016-04-15');
+INSERT INTO `marcas` (`id_mca`, `nomb_mca`, `origen_mca`, `prov_mca`, `tel_mca`, `nota_mca`, `fecha_reg`) VALUES
+(29, 'Deltatrak', 'Nacional', 'Deltatrak International Mexico', '7373737373', 'eejjjej', '2016-04-11'),
+(31, 'Extruflex', 'ImportaciÃ³n', '', '', '', '2016-04-11'),
+(32, 'Taylor', 'Nacional', '', '', '', '2016-04-12'),
+(33, 'Extech', 'Nacional', '', '', '', '2016-04-12'),
+(34, 'Escort Ilog', 'ImportaciÃ³n', '', '', '', '2016-04-14'),
+(37, 'Dare', 'ImportaciÃ³n', 'Dare Products Inc', '888', 'Atn: Bob', '2016-04-17');
 
 -- --------------------------------------------------------
 
@@ -101,7 +102,9 @@ INSERT INTO `productos` (`id_prod`, `nomb_prod`, `desc_prod`, `mca_prod`, `orige
 (4, '365510', 'Cronometro digital', 'Extech', 'Nacional', 'Activo', 'se usa en plantas de proceso', 200, 50, 300, 350, '2016-04-12'),
 (8, '11050', 'Termometro tipo paleta', 'Deltatrak', 'Nacional', 'Activo', 'aaaaa', 400, 50, 600, 650, '2016-04-15'),
 (10, '20901', 'Graficador reusable usb', 'Deltatrak', 'Nacional', 'Activo', 'grafica temperatura', 1000, 30, 1300, 1400, '2016-04-17'),
-(11, '1523', 'Termometro digital', 'Avaly', 'Nacional', 'Inactivo', 'sss', 100, 20, 120, 222, '2016-04-17');
+(11, '1523', 'Termometro digital', 'Avaly', 'Nacional', 'Inactivo', 'sss', 100, 20, 120, 222, '2016-04-17'),
+(12, '62Max', 'Termometro infrarrojo digital', 'Fluke', 'Nacional', 'Activo', 'uso de lejos', 100, 200, 300, 350, '2016-04-17'),
+(14, 'aaaaaa', 'aaaaaaa', 'Extech', 'Nacional', 'Activo', 'adaa', 100, 20, 120, 2332, '2016-04-17');
 
 --
 -- Índices para tablas volcadas
@@ -133,17 +136,17 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_cte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `marcas`
 --
 ALTER TABLE `marcas`
-  MODIFY `id_mca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_mca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- js/myjava.js
+++ js/myjava.js
@@ -288,6 +288,7 @@ function editarProducto(id){
 				$('#costProd').val(datos[6]);
 				$('#utilidadProd').val(datos[7]);
 				$('#precProd').val(datos[8]);
+				$('#precRec').val(datos[9]);
 				$('#registra-producto').modal({
 					show:true,
 					backdrop:'static'
